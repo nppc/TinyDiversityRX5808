b61df50a65d329a3d1c72cc878ed2c2b731323e4
Palonen <ppalonen@bethel.jw.org>
2017-11-21 13:30:43 +0100

    Added signal switching routine

--- TinyDivercityFirmware/TinyDivercityFirmware.ino
+++ TinyDivercityFirmware/TinyDivercityFirmware.ino
@@ -1,7 +1,7 @@
 
 #include <EEPROM.h>
 
-#define MIN_TUNE_TIME 25 // update RSSI values every 25ms
+//#define MIN_TUNE_TIME 25 // update RSSI values every 25ms
 
 // RSSI strength should be greater than the value below (percent) over the
 // other receiver before we switch. This pervents flicker when RSSI values
@@ -10,7 +10,7 @@
 
 // How long (ms) the RSSI strength has to have a greater difference than the
 // above before switching.
-#define DIVERSITY_HYSTERESIS_PERIOD 5
+#define DIVERSITY_HYSTERESIS_PERIOD 10
 
 
 #define PIN_SWITCH_VIDEO 	0	//PB0
@@ -27,6 +27,7 @@
 #define EEPROM_MAX_RSSI_B_ADDR	13	// RSSI RAW Calibration value (int)
 
 uint8_t activeReceiver;
+uint8_t diversityTargetReceiver;
 uint8_t rssiA = 0;
 uint16_t rssiARaw = 0;
 uint8_t rssiB = 0;
@@ -36,6 +37,8 @@ uint16_t rssiAMin;
 uint16_t rssiAMax;
 uint16_t rssiBMin;
 uint16_t rssiBMax;
+
+unsigned long diversityHysteresisTimer;
 
 
 enum ReceiverSelelct: byte {RX_A, RX_B};
@@ -86,7 +89,9 @@ void setup()
 		EEPROM.update(EEPROM_RESET_FLAG_ADDR, 1); //clear RESET flag to enter calibration mode after new RESET
 	}
 
-    //setActiveReceiver(RX_A);
+    setActiveReceiver(RX_A);
+	diversityTargetReceiver = activeReceiver;	// Initialize variable for diversity switching
+	
 	readEEPROMSettings();
 
 }
@@ -134,54 +139,43 @@ uint16_t updateRssi() {
 }
 	
 void switchDiversity() {
-/*
-	ReceiverId nextReceiver = activeReceiver;
 
-	if (EepromSettings.diversityMode == DiversityMode::AUTO) {
-		int8_t rssiDiff = (int8_t) rssiA - (int8_t) rssiB;
-		uint8_t rssiDiffAbs = abs(rssiDiff);
-		ReceiverId currentBestReceiver = activeReceiver;
+	int8_t rssiDiff = (int8_t) rssiA - (int8_t) rssiB;
+	uint8_t rssiDiffAbs = abs(rssiDiff);
+	uint8_t currentBestReceiver;
+	uint8_t nextReceiver;
+	
+	nextReceiver = activeReceiver;
 
-		if (rssiDiff > 0) {
-			currentBestReceiver = ReceiverId::A;
-		} else if (rssiDiff < 0) {
-			currentBestReceiver = ReceiverId::B;
-		} else {
-			currentBestReceiver = activeReceiver;
-		}
+	if (rssiDiff > 0) {
+		currentBestReceiver = RX_A;
+	} else if (rssiDiff < 0) {
+		currentBestReceiver = RX_B;
+	} else {
+		currentBestReceiver = activeReceiver;
+	}
 
-		if (rssiDiffAbs >= DIVERSITY_HYSTERESIS) {
-			if (currentBestReceiver == diversityTargetReceiver) {
-				if (diversityHysteresisTimer.hasTicked()) {
-					nextReceiver = diversityTargetReceiver;
-				}
-			} else {
-				diversityTargetReceiver = currentBestReceiver;
-				diversityHysteresisTimer.reset();
+	if (rssiDiffAbs >= DIVERSITY_HYSTERESIS) {
+		if (currentBestReceiver == diversityTargetReceiver) {
+			if ((diversityHysteresisTimer+DIVERSITY_HYSTERESIS_PERIOD)<millis()) {
+				nextReceiver = diversityTargetReceiver;
 			}
 		} else {
-			diversityHysteresisTimer.reset();
+			diversityTargetReceiver = currentBestReceiver;
+			diversityHysteresisTimer=millis();
 		}
 	} else {
-		switch (EepromSettings.diversityMode) {
-			case DiversityMode::FORCE_A:
-				nextReceiver = ReceiverId::A;
-				break;
-
-			case DiversityMode::FORCE_B:
-				nextReceiver = ReceiverId::B;
-				break;
-		}
+		diversityHysteresisTimer=millis();
 	}
 
 	setActiveReceiver(nextReceiver);
-	*/
+
 }
 	
 void loop() {
-    
+    // Update RSSI values as fast as possible.
 	updateRssi();
-	switchDiversity();
+	switchDiversity(); // Will switch only if signals are significally different and difference stays for a while.
 }
 
 void readEEPROMSettings(){
@@ -198,9 +192,16 @@ int readEEPROMint(byte addr){
 void doCalibration(){
 	// at the moment lets just eneter DEBUG mode (switch video source every second)
 	while(1==1){
-		delay(1000);
+		//delay(1000);
+		mDelay(1000);
 		setActiveReceiver(RX_A);
-		delay(1000);
+		//delay(1000);
+		mDelay(1000);
 		setActiveReceiver(RX_B);
 	}
+}
+
+void mDelay(int dtmp){
+unsigned long tmp = millis();
+while(tmp+dtmp>millis()){}
 }
